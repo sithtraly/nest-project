4a8e4842ad26a80aa0bd7af9b043d5746450d646
Sith Traly <sithtraly147@gmail.com>
2023-11-08 12:50:10 +0700

    Add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3000
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Project')
+    .setDescription('Rest API for nest project, project to controller classroom')
+    .setVersion('1.0')
+    .build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, swaggerDocument)
   await app.listen(port, () => console.log('Server is running at:', 'http://localhost:' + port));
 }
 bootstrap();
